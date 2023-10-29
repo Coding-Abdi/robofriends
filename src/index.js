0542b97d0a97dba134337c7978cdf610b216f47b
Coding-Abdi <abdi.brum@gmail.com>
2023-10-29 15:38:33 +0000

    created card component for robot profiles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <CardList robots={robots}/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
